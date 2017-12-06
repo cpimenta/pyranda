27987cace97697af685cd4f956e0a949b117cdb2
Britton Olson <olson45@llnl.gov>
2017-12-05 20:16:16 -0800

    Adding setup and renaming src

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pyranda',
+    version='0.1dev',
+    packages=['pyranda',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
